0600785c8b3377824175325bfc16a353cabb16ae
eric <eric@serrulla.com>
2018-02-13 11:33:02 +0100

    answers added

--- app/src/main/java/com/serrulla/eric/quiz/QuizActivity.java
+++ app/src/main/java/com/serrulla/eric/quiz/QuizActivity.java
@@ -2,12 +2,29 @@ package com.serrulla.eric.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
 
-    @Override
+    private final int[] button_ids = {
+            R.id.answer1, R.id.answer2, R.id.answer3, R.id.answer4
+    };
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        TextView question_text = findViewById(R.id.question_text);  //R is for resource
+        question_text.setText(R.string.question_content);
+
+        String[] answer_texts = getResources().getStringArray(R.array.answer_texts);
+
+        for (int i = 0; i < answer_texts.length; i++){
+            RadioButton rb = findViewById(button_ids[i]);
+            rb.setText(answer_texts[i]);
+        }
+
+
     }
 }
